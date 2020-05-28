cb4c0800b3ba77ace9de205b7f351874e08d8e0f
Gustavo Carvalho <gusilva@padtec.com.br>
2020-05-28 10:14:47 -0300

    1014

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,7 +3,6 @@ module.exports = function (app) {
         const connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.produtosDAO(connection);
         produtosDAO.lista(function (erros, resultados) {
-            console.log(resultados);
             res.render('home/index', {livros: resultados});
         });
         connection.end();
